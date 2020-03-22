4fc1d8ba49290712e9432a1513b83b6d637aeea4
Tobias Thrane <tkthrane_93@hotmail.com>
2020-03-22 21:01:03 +0100

    Initial commit

---
+++ level-1.js
@@ -0,0 +1,91 @@
+// question 1
+
+// var name = "Tobias";
+
+// question 2
+
+// var person = {
+//    name: "Tobias",
+//    age: 27
+//   } 
+
+// question 3
+
+// var outOfStock = true;
+// 
+// if(outOfStock === true) {
+//     console.log("Out of stock")
+// } else {
+//     console.log("In stock")
+// }
+
+// question 4
+
+// var numbers = [5,6,10,25,36];
+// 
+// console.log(numbers);
+
+// question 5
+
+// for(let i = 15; i <26; i++) {
+//     console.log (i);
+// }
+
+// question 6
+
+// for(let i = 20; i <= 20; i++) {
+//          console.log (i);
+// }
+
+// question 7
+
+// var users = [
+//     {
+//         name: "John Doe",
+//         age: 35,
+//         subscribed: true
+//     },
+//     {
+//         name: "Alice More",
+//         age: 29,
+//         subscribed: false
+//     }
+// 
+// ];
+// 
+// for(var i = 0; i < users.length; i++) {
+//     console.log(users[i]);
+// }
+// 
+// console.log(users[0].age);
+// console.log(users[0].subscribed);
+// console.log(users[1].age);
+// console.log(users[1].subscribed);
+
+// question 8
+
+// function whatIDontLike(dontLike) {
+//     console.log("I don't like " + dontLike);
+// }
+// 
+// whatIDontLike("fastfood");
+
+// question 9
+
+// function subT(num1, num2) {
+//     console.log(num1 - num2);
+// }
+// 
+// subT(10,5);
+
+// question 10
+
+// var empty = []; 
+// 
+// function add() {
+//     empty.push(0);
+// }
+// 
+// add();
+// 
+// console.log(empty);
